bb0eb3294c23839e661b592e2e0f5db8e1b954fb
OGtrilliams <tribecca@tribecc.us>
2022-03-22 11:47:15 -0400

    fix for krew installer script

--- troubleshoot-kubecon/preflight/assets/krew.sh
+++ troubleshoot-kubecon/preflight/assets/krew.sh
@@ -5,20 +5,24 @@
 #Description	:Installer script for the kubectl krew plugin
 #Args		:
 #Author		:T. Nichole Williams
-#Email		:tribecca@tribecc.us
+#Email		:trilliams@replicated.com
 #GitHub		:@OGtrilliams
 #Website	:https://krew.sigs.k8s.io/docs/user-guide/setup/install/
 ##############################################
+# step 1 - Download krew plugin compatible with host OS
 (
   set -x; cd "$(mktemp -d)" &&
-  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.tar.gz" &&
-  tar zxvf krew.tar.gz &&
-  KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_$(uname -m | sed -e 's/x86_64/amd64/' -e 's/arm.*$/arm/')" &&
-  "$KREW" install krew
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
 )
+
 # step 2 - add path to BASH
 echo "export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"" >> $HOME/.bashrc
 # step 3 - profit
 sleep 3
-source /root/.bashrc
+source $HOME/.bashrc
 echo "Installation is complete. You are now ready to begin using krew."
