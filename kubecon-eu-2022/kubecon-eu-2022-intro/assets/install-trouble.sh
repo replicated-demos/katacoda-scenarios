6e8a17fc054adb3eedc07646790eaf74b283f839
AJ <aj@replicated.com>
2022-04-29 13:41:34 -0400

    Update install-trouble.sh to wait.

--- kubecon-eu-2022/kubecon-eu-2022-intro/assets/install-trouble.sh
+++ kubecon-eu-2022/kubecon-eu-2022-intro/assets/install-trouble.sh
@@ -2,9 +2,18 @@
 
 FILE=/usr/bin/sbctl
 if [[ -f "$FILE" ]]; then
-  echo "CHAOS IS READY!!!!"
-  sbctl shell -s supportbundle-example.tar.gz
+
+  ps aux | grep -i sbctl | grep -v grep
+
+  if [[ $? -eq 1 ]]; then
+    echo "CHAOS IS READY!!!!"
+    sbctl shell -s supportbundle-example.tar.gz
+
+  else
+    echo "There is already too much chaos :)"
+  fi
+
 else
-  echo "Wait for cluster to become chaotic..."
+  echo "Wait for cluster to become chaotic...try again..."
 fi
 
